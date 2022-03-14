57d271bbd1985c3ae624454ffe84ad38a2918fd9
Mauro Gonzalo Gutierrez <maurog7gool@hotmail.com>
2022-03-14 19:11:25 -0300

    finalizado con promises

--- app.js
+++ app.js
@@ -3,48 +3,77 @@ const path = require("path");
 
 // const alumno1 = fs.readFileSync('./files/alumno1.json', 'utf-8')
 
-fs.readFile("./files/alumno1.json", "utf8", function (err1, data1) {
-  fs.readFile("./files/alumno2.json", "utf8", function (err2, data2) {
-    fs.readFile("./files/alumno3.json", "utf8", function (err3, data3) {
-      fs.readFile("./files/alumno4.json", "utf8", function (err4, data4) {
-        fs.readFile("./files/alumno5.json", "utf8", function (err5, data5) {
-          if (err1 || err2 || err3 || err4 || err5) {
-            throw new Error();
-          }
-          let data = data1 + data2 + data3 + data4 + data5;
-          fs.writeFile("listadodeAlumnos.json", data, function (err) {
-            if (err) throw err;
-            console.log("Archivos escritos con éxito\n");
-          });
-        });
-      });
-    });
-  });
-});
-
-// alumnos.forEach((item) =>
-// fs.readFile(item, (err,data) => {
-//   if (err) throw err;
-//   fs.writeFile(`${item}.txt`, data, (err) => {
-//     if (err) throw err,
-//     console.log("Archivos escritos con éxito\n");
-//   })
+// fs.readFile("./files/alumno1.json", "utf8", function (err1, data1) {
+//   fs.readFile("./files/alumno2.json", "utf8", function (err2, data2) {
+//     fs.readFile("./files/alumno3.json", "utf8", function (err3, data3) {
+//       fs.readFile("./files/alumno4.json", "utf8", function (err4, data4) {
+//         fs.readFile("./files/alumno5.json", "utf8", function (err5, data5) {
+//           if (err1 || err2 || err3 || err4 || err5) {
+//             throw new Error();
+//           }
+//           let data = data1 + data2 + data3 + data4 + data5;
+//           fs.writeFile("listadodeAlumnos.json", data, function (err) {
+//             if (err) throw err;
+//             console.log("Archivos escritos con éxito\n");
+//           });
+//         });
+//       });
+//     });
+//   });
 // });
 
-//---intento con for each
-
-// const json1 = "./files/alumno1.json",
-// json2 = "./files/alumno2.json",
-// json3 = "./files/alumno3.json";
-// json4 = "./files/alumno4.json";
-// json5 = "./files/alumno5.json";
-
-// const alumnos = [json1, json2, json3,json4,json5]
-
-// fs.readFile(path.join(__dirname, 'files', 'alumno1.json','alumno2.json'), 'utf8', (err,data) => {
-//   if (err) throw err;
-//    fs.writeFile(path.join(__dirname, 'listadodeAlumnos.json'), data, (err) => {
-//   if (err) throw err;
-//   console.log('write Complete')
-// })
-// })
+const student1 = new Promise((resolve, reject) => {
+  fs.readFile("./files/alumno1.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      resolve(JSON.parse(data));
+    }
+  });
+});
+const student2 = new Promise((resolve, reject) => {
+  fs.readFile("./files/alumno2.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      resolve(JSON.parse(data));
+    }
+  });
+});
+const student3 = new Promise((resolve, reject) => {
+  fs.readFile("./files/alumno3.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      resolve(JSON.parse(data));
+    }
+  });
+});
+const student4 = new Promise((resolve, reject) => {
+  fs.readFile("./files/alumno4.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      resolve(JSON.parse(data));
+    }
+  });
+});
+const student5 = new Promise((resolve, reject) => {
+  fs.readFile("./files/alumno5.json", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      resolve(JSON.parse(data));
+    }
+  });
+});
+const createList = (datos) => {
+  fs.writeFile("listadodeAlumnos.json", datos, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
+Promise.all([student1, student2, student3, student4, student5])
+  .then((data) => createList(JSON.stringify(data, null, 2)))
+  .catch((err) => console.log(err));
